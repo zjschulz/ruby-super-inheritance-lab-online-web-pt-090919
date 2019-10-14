85c14a9f3b21c589040055a54e0b12f801c14d8b
Zachary Schulz <zjschulz@gmail.com>
2019-10-14 00:27:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student 
   
   def hello 
-  "Hey there! I'm so excited to learn stuff."
+    "Hey there! I'm so excited to learn stuff."
   end
   
   def raise_hand 
-  "Pick me!"
+    "Pick me!"
   end
   
 end
