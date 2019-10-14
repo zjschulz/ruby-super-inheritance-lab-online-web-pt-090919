3aeeab859f46908a99b22f5ce94c116e32084ae9
Zachary Schulz <zjschulz@gmail.com>
2019-10-14 00:27:38 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,10 +1,12 @@
-class ChattyStudent 
+class ChattyStudent < Student
   
   def hello 
+    super
   "Hey there! I'm so excited to learn stuff."
   end
   
   def raise_hand 
+    super
   "Pick me!"
   end
   
